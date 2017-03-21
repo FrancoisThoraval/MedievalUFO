0811ed18c0066f57e3db730a55b2ff629ac55be5
ThePanda33 <francoisthoraval@hotmail.fr>
2017-03-21 14:22:53 +0100

    event

--- Game/splashScreen.cpp
+++ Game/splashScreen.cpp
@@ -13,7 +13,7 @@ void SplashScreen::show(sf::RenderWindow &window){
      sf::Event event;
      while (true) {
           while (window.pollEvent(event)) {
-               if (event.type == sf::Event::EventType::KeyPressed || event.type == sf::Event::EventType::MouseButtonPressed || event.type == sf::Event::EventType::Closed) {
+               if (event.type == sf::Event::KeyPressed || event.type == sf::Event::MouseButtonPressed || event.type == sf::Event::Closed) {
                     return;
                }
           }
